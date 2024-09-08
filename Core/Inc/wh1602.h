4f6c235e67784f22474ec20fd4740ead1954bdc7
Ilshat <aminoviilshat93@gmail.com>
2024-09-08 15:59:25 +0300

    Добавил экран 1602

---
+++ Core/Inc/wh1602.h
@@ -0,0 +1,27 @@
+
+#include "stm32f1xx_hal.h"
+#include "main.h"
+#ifndef wh1602
+#define wh1602
+
+#define     PIN_RS            RS_Pin          // PB12
+#define     PIN_RS_PORT       RS_GPIO_Port               
+#define     PIN_EN            EN_Pin          // PA10
+#define     PIN_EN_PORT       EN_GPIO_Port               
+#define     PIN_D7            DB7_Pin          // PA11
+#define     PIN_D7_PORT       DB7_GPIO_Port               
+#define     PIN_D6            DB6_Pin          // PA12
+#define     PIN_D6_PORT       DB6_GPIO_Port               
+#define     PIN_D5            DB5_Pin          // PA8
+#define     PIN_D5_PORT       DB5_GPIO_Port               
+#define     PIN_D4            DB4_Pin          // PA9
+#define     PIN_D4_PORT       DB4_GPIO_Port               
+
+
+void PulseLCD(void);
+void SendByte(char ByteToSend, int IsData);
+void Cursor(char Row, char Col);
+void ClearLCDScreen(void);
+void InitializeLCD(void);
+void PrintStr(char *Text);
+#endif
